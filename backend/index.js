d32accb3808c59cbc6d02ff774d268b857803b80
Jawwad <jawwadumar99@gmail.com>
2023-08-08 12:06:07 +0530

    created the auth and notes

--- backend/index.js
+++ backend/index.js
@@ -5,9 +5,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+//Availbale Routes
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
+
+//Default expreess template->
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
