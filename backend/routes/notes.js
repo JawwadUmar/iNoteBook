fd5c8edd7032e27be72346b7552f3a851e4ad05e
Jawwad <jawwadumar99@gmail.com>
2023-08-11 21:35:38 +0530

    router 3 added to update existing notes using router.put

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -43,4 +43,35 @@ router.post('/addnote', fetchuser,  [
   }
 })
 
+
+//ROUTE 3: UPDATING A NEW NOTE using PUT "/notes/updatenote"  LOGIN REQUIRED
+router.put('/updatenote/:id', fetchuser, async (req, res) => {
+
+  const {title, description, tag} = req.body;
+
+  //Create a new note object
+
+  const newNote = {};
+
+  if(title){newNote.title = title};
+  if(description){newNote.description = description};
+  if(tag){newNote.tag = tag};
+
+  //Find the note to be updated and update it
+  // const note = Note.findByIdAndUpdate()
+  let note = await Note.findById(req.params.id);
+  
+  if(!note){
+    return res.status(404).send("Not found");
+  }
+
+  if(note.user.toString() !== req.user.id){
+    return res.status(401).send("Not Allowed");
+  }
+
+  note = await Note.findByIdAndUpdate(req.params.id, {$set: newNote}, {new:true});
+  res.json({note});
+  
+})
+
 module.exports = router
