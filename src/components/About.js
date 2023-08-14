77d0055963cccca26e702755a9222dd2449ae2a7
Jawwad <jawwadumar99@gmail.com>
2023-08-14 05:33:32 +0530

    use location hook to brighten the buttons, added form

--- src/components/About.js
+++ src/components/About.js
@@ -3,17 +3,9 @@ import { useContext } from 'react'
 import noteContext from '../context/notes/noteContext'
 
 const About = () => {
-  const a = useContext(noteContext)
-
-  //using useEffect Hook
-
-  useEffect(() =>{
-    a.update();
-    // eslint-disable-next-line
-  }, [])
 
   return (
-    <div>This is About {a.state.name} and he is in class {a.state.class}</div>
+    <div>This is About</div>
   )
 }
 
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,22 +2,9 @@ import NoteContext from "./noteContext";
 import { useState } from "react";
 
 const NoteState =(props) =>{
-    const s1 = {
-        "name": "jae",
-        "class": "5b"
-    }
-
-    const [state, setState] = useState(s1);
-    const update= () =>{
-        setTimeout(() => {
-            setState({
-                "name" : "Larry",
-                "class": "10b"
-            })
-        }, 1000)
-    }
+   
     return(
-        <NoteContext.Provider value= {{state, update}}>
+        <NoteContext.Provider value= {{}}>
             {props.children}
         </NoteContext.Provider>
     )
